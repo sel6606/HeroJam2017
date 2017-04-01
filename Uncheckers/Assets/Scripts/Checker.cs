bd2772ecc5a9296d922837fc15fa7209825b9ea2
MAIN\sel6606 <sel6606@g.rit.edu>
2017-04-01 11:24:16 -0400

    Fixed some errors and moved the Checker code to the correct script

--- Uncheckers/Assets/Scripts/Checker.cs
+++ Uncheckers/Assets/Scripts/Checker.cs
@@ -2,29 +2,106 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checker : MonoBehaviour {
+public enum cColor { Black, Red, Unknown };
+
+public class Checker : MonoBehaviour
+{
 
     //Color
     //Whether or not it is flipped
     //Index in the 2D array
 
-    // Use this for initialization
-    void Start() {
+    private Vector3 positionChecker;
+
+    public Vector3 PositionChecker
+    {
+
+        get { return gameObject.transform.position; }
+
+        set { positionChecker = value; }
+
+    }
+
+    private cColor checkerColor;
+
+    public cColor CheckerColor
+    {
+
+        get { return checkerColor; }
+
+        set { checkerColor = value; }
+
+    }
+
+    private bool flipped;
+
+    public bool Flipped
+    {
+
+        get { return flipped; }
+
+        set { flipped = value; }
+
+    }
+
+    private int indX;
+
+    public int indexX
+    {
+
+        get { return indX; }
+
+        set { indX = value; }
+
+    }
+
+    private int indY;
+
+    public int indexY
+    {
+
+        get { return indY; }
+
+        set { indY = value; }
+
+    }
+
+    void Start()
+    {
+
+    }
+    //Creates a checker
+    void Create(cColor color, int posX, int posY, bool upFace)
+    {
+
+        checkerColor = color;
+
+        indexX = posX;
+        indexY = posY;
+
+        Flipped = upFace;
 
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
+
+
 
     }
 
     void Move()
     {
 
+
+
     }
 
     void Jump()
     {
+
+
 
     }
 }
