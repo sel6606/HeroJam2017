6399ac05593e8f806eee043564475dc429622ca4
MAIN\jfw6252 <jfw6252@g.rit.edu>
2017-04-01 21:13:11 -0400

    Fixed the problems

--- Uncheckers/Assets/Scripts/lightClickMoveScript.cs
+++ Uncheckers/Assets/Scripts/lightClickMoveScript.cs
@@ -31,38 +31,40 @@ public class lightClickMoveScript : MonoBehaviour {
 
 		if (!checkScri.selected.GetComponent<Checker> ().animate.isPlaying) 
 		{
-			if(Mathf.Abs((float)(checkScri.selected.GetComponent<Checker>().indexX - inX)) == 1)
-			{
-				if (hasAnimated == false) 
-				{
-					checkScri.selected.GetComponent<Checker> ().AnimateChecker ("Slide");
-					hasAnimated = true;
-				} 
-				else 
-				{
-					checkScri.selected.GetComponent<Checker>().Move(inX, inY, gameObject.transform.position);
+            if (Mathf.Abs((float)(checkScri.selected.GetComponent<Checker>().indexX - inX)) == 1)
+            {
+                if (hasAnimated == false)
+                {
+                    checkScri.selected.GetComponent<Checker>().AnimateChecker("Slide");
+                    hasAnimated = true;
+                }
+                else
+                {
+                    checkScri.selected.GetComponent<Checker>().Move(inX, inY, gameObject.transform.position);
 
-					checkScri.Deselect(checkScri.selected);
+                    checkScri.Deselect(checkScri.selected);
 
-					hasAnimated = false;
-				}
-			}
-			else
-			{
-				if (hasAnimated == false) 
-				{
-					checkScri.selected.GetComponent<Checker> ().AnimateChecker ("Jump");
-					hasAnimated = true;
-				} 
-				else
-				{
-					checkScri.selected.GetComponent<Checker>().Jump(inX, inY, gameObject.transform.position);
+                    hasAnimated = false;
+                }
+            }
 
-					checkScri.PostMove(checkScri.selected);
+            else
+            {
+                if (hasAnimated == false)
+                {
+                    checkScri.selected.GetComponent<Checker>().AnimateChecker("Jump");
+                    hasAnimated = true;
+                }
+                else
+                {
+                    checkScri.selected.GetComponent<Checker>().Jump(inX, inY, gameObject.transform.position);
 
-					hasAnimated = false;
-				}
-			}	
+                    checkScri.PostMove(checkScri.selected);
+
+                    hasAnimated = false;
+                }
+            }	
 		}
     }
 }
+
